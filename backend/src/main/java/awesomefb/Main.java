a0b5dcbe245d01dbd94794e3fd0ed35d52ed2f7f
earlwlkr <mike358015@gmail.com>
2015-05-25 16:25:34 +0700

    Change the get user data strategy
    No longer saves user data in the same posts collection, instead saves to another collect (users), reference the _id to the creator field. Doing this, we can update user data respectively, like after crawling all posts.

--- backend/src/main/java/awesomefb/Main.java
+++ backend/src/main/java/awesomefb/Main.java
@@ -6,14 +6,10 @@ package awesomefb;
 import java.net.UnknownHostException;
 import java.util.List;
 
+import com.mongodb.*;
 import org.springframework.social.UncategorizedApiException;
 import org.springframework.social.facebook.api.*;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.MongoClient;
 
 /**
  * @author earl
@@ -28,7 +24,8 @@ public class Main {
    * @throws UnknownHostException 
    */
   public static void main(String[] args) throws UnknownHostException {
-    String rootPage = "yannews";
+    // Topics: samsung, iphone, coffee, galaxy
+    String rootPage = "KHTNCFS";
     String accessToken = "CAAJK2NXosZAABAPjvopDAeoFgmBFZB98ixFdop1sFGiBKsZB2ZAZCrPgVein9u6lxxG28fDHELyvE1Unyvmi2XSkaG5wuIflAYTc2vp7DT4xkD6j9lFN1l60la0sVsLbPRysxbOV6kmbAKpLPXbvza4Rb1bW5k9gu0ZBZARbI4ZBcUYwlb5Wat2OhZC4zaqEoXZCDSLAy6CTZBSvXtNujkGRipF";
     
     facebook = new FacebookTemplate(accessToken);
@@ -36,64 +33,54 @@ public class Main {
     MongoClient mongoClient = new MongoClient();
     DB db = mongoClient.getDB("awesomefb");
     // Connect to collection (table in mysql).
-    DBCollection collection = db.getCollection("posts");
-    collection.drop();
+    DBCollection posts_collection = db.getCollection("posts");
+    DBCollection users_collection = db.getCollection("users");
+    posts_collection.drop();
+    users_collection.drop();
     
     List<Post> feed = facebook.feedOperations().getPosts(rootPage);
     for (Post post: feed) {
       String id = post.getId();
-      System.out.println("* Processing post " + id);
+      System.out.println("[awesomeFb] Processing post " + id);
       
       BasicDBObject doc = new BasicDBObject("message", post.getMessage())
-                                            .append("creator", getSenderDetailedInfo(post.getFrom().getId()))
+                                            .append("creator", getCreatorInfo(users_collection, post.getFrom()))
                                             .append("time", post.getCreatedTime())
-                                            .append("comments", getComments(facebook.commentOperations()
-                                                                                    .getComments(id)))
-                                            .append("link", "https://facebook.com/" + post.getId());
-      collection.insert(doc);
+                                            .append("comments", getComments(users_collection, facebook.commentOperations()
+                                                    .getComments(id)))
+                                            .append("link", post.getLink());
+      posts_collection.insert(doc);
     }
   }
     
   /**
-   * @param id
-   * @return
+   * Saves user data into users collection,
+   * returns DBObject with newly inserted id and some basic info (FB ID, name)
+   * @param users_collection
+   * @param reference
+   * @return BasicDBObject
    */
-  private static BasicDBObject getSenderDetailedInfo(String id) {
-    BasicDBObject ret = new BasicDBObject("id", id);
-    try {
-      Page page = facebook.pageOperations().getPage(id);
-      String name = page.getName();
-      ret.append("name", name);
+  private static BasicDBObject getCreatorInfo(DBCollection users_collection, Reference reference) {
+    String id = reference.getId();
+    String name = reference.getName();
 
-      List<Reference> cat = page.getCategoryList();
-      if (cat != null) {
-        ret.append("cat", cat.get(0).getName());
-      }
+    BasicDBObject userObject = new BasicDBObject("id", id).append("name", name);
+    System.out.println("[awesomeFb] Inserting basic user data " + id);
+    users_collection.insert(userObject);
 
-    } catch (UncategorizedApiException e) { // It's a page, extract page's info.
-      User user = facebook.userOperations().getUserProfile(id);
-      String name = user.getName();
-      ret.append("name", name);
-
-      String email = user.getEmail();
-      if (email != null) {
-        ret.append("email", email);
-      }
-
-      String birthday = user.getBirthday();
-      if (birthday != null) {
-        ret.append("birthday", birthday);
-      }
-    }
+    // Returns DBObject with id of the object just inserted
+    BasicDBObject ret = new BasicDBObject("user_id", userObject.get("_id").toString())
+            .append("fb_id", id).append("name", name);
     return ret;
   }
   
-  private static BasicDBObject getComments(List<Comment> comments) {
+  private static BasicDBObject getComments(DBCollection users_collection, List<Comment> comments) {
     Integer count = comments.size();
     BasicDBObject ret = new BasicDBObject("count", count);
     for (Integer i = 0; i != count; i++) {
-      ret.append(i.toString(), new BasicDBObject("creator", getSenderDetailedInfo(comments.get(i).getFrom().getId()))
-                            .append("message", comments.get(i).getMessage()));
+      ret.append(i.toString(), new BasicDBObject("creator", getCreatorInfo(users_collection,
+              comments.get(i).getFrom()))
+              .append("message", comments.get(i).getMessage()));
     }
     return ret;
   }
