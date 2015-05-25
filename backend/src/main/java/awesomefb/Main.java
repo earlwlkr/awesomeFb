8bbddc41dea68e340f1fc2c82364c2384eca09ae
earlwlkr <mike358015@gmail.com>
2015-05-25 18:07:12 +0700

    Indent 4 spaces

--- backend/src/main/java/awesomefb/Main.java
+++ backend/src/main/java/awesomefb/Main.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package awesomefb;
 
@@ -10,30 +10,29 @@ import org.springframework.social.facebook.api.Post;
 
 /**
  * @author earl
- *
  */
 public class Main {
 
-  private static DatabaseManager mDatabaseManager;
-  
-  /**
-   * @param args
-   * @throws UnknownHostException 
-   */
-  public static void main(String[] args) throws UnknownHostException {
-    // Topics: samsung, iphone, coffee, galaxy
-    String rootPage = "KHTNCFS";
+    private static DatabaseManager mDatabaseManager;
 
-    // Connect to mongodb.
-    mDatabaseManager = DatabaseManager.getInstance();
-    
-    List<Post> feed = FacebookManager.getInstance().getPosts(rootPage);
-    for (Post post: feed) {
-      String id = post.getId();
-      System.out.println("[awesomeFb] Processing post " + id);
-      
-      ExtractedPost extractedPost = new ExtractedPost(post);
-      mDatabaseManager.insertPost(extractedPost);
+    /**
+     * @param args
+     * @throws UnknownHostException
+     */
+    public static void main(String[] args) throws UnknownHostException {
+        // Topics: samsung, iphone, coffee, galaxy
+        String rootPage = "KHTNCFS";
+
+        // Connect to mongodb.
+        mDatabaseManager = DatabaseManager.getInstance();
+
+        List<Post> feed = FacebookManager.getInstance().getPosts(rootPage);
+        for (Post post : feed) {
+            String id = post.getId();
+            System.out.println("[awesomeFb] Processing post " + id);
+
+            ExtractedPost extractedPost = new ExtractedPost(post);
+            mDatabaseManager.insertPost(extractedPost);
+        }
     }
-  }
 }
