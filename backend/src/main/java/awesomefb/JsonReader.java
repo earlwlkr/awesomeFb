1bf21260b18961faa45d4b551a58872c58f3ed98
earlwlkr <mike358015@gmail.com>
2015-05-26 15:10:30 +0700

    Print states

--- backend/src/main/java/awesomefb/JsonReader.java
+++ backend/src/main/java/awesomefb/JsonReader.java
@@ -1,6 +1,7 @@
 package awesomefb;
 
 import java.io.IOException;
+import java.net.SocketException;
 import java.net.URL;
 
 import org.json.JSONException;
@@ -9,7 +10,9 @@ import org.json.JSONObject;
 public class JsonReader {
 
     public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        System.out.println("Waiting for HTTP response from " + url);
         String jsonText = org.apache.commons.io.IOUtils.toString(new URL(url).openStream());
+        System.out.println("Received HTTP response.");
         JSONObject json = new JSONObject(jsonText);
         return json;
     }
