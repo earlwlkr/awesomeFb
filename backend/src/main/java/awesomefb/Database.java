fd156e409a1b26fac6f3afc6b919982e50e227e8
earlwlkr <mike358015@gmail.com>
2015-05-29 08:40:00 +0700

    Processed and queue collection operations

--- backend/src/main/java/awesomefb/Database.java
+++ backend/src/main/java/awesomefb/Database.java
@@ -1,11 +1,16 @@
 package awesomefb;
 
 import awesomefb.facebook.Comment;
+import awesomefb.facebook.Page;
 import awesomefb.facebook.Post;
 import awesomefb.facebook.User;
 import com.mongodb.*;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by earl on 5/25/2015.
@@ -15,6 +20,8 @@ public class Database {
     private DB database;
     private DBCollection postsCollection;
     private DBCollection usersCollection;
+    private DBCollection processedCollection;
+    private DBCollection queueCollection;
 
     public static Database getInstance() {
         if (instance == null) {
@@ -35,9 +42,54 @@ public class Database {
 
         postsCollection = this.database.getCollection("posts");
         usersCollection = this.database.getCollection("users");
+        processedCollection = this.database.getCollection("processed");
+        queueCollection = this.database.getCollection("queue");
+    }
 
-        postsCollection.drop();
-        usersCollection.drop();
+    public void drop() {
+        database.dropDatabase();
+    }
+
+    public void insertProcesed(String id) {
+        processedCollection.insert(new BasicDBObject("fb_id", id));
+    }
+
+    public void insertQueue(Page page) {
+        queueCollection.insert(new BasicDBObject("fb_id", page.getFacebookId())
+                .append("name", page.getName()));
+    }
+
+    public List<String> getProcessed() {
+        List<String> processedIds = new ArrayList<String>();
+        DBCursor cursor = processedCollection.find();
+        try {
+            while (cursor.hasNext()) {
+                processedIds.add(cursor.next().get("fb_id").toString());
+            }
+        } finally {
+            cursor.close();
+        }
+        return processedIds;
+    }
+
+    public Queue<Page> getQueue() {
+        Queue<Page> queue = new LinkedList<>();
+        DBCursor cursor = queueCollection.find();
+        try {
+            while (cursor.hasNext()) {
+                DBObject obj = cursor.next();
+                queue.add(new Page(obj.get("fb_id").toString(), obj.get("name").toString()));
+            }
+        } finally {
+            cursor.close();
+        }
+        return queue;
+    }
+
+    public void removeFromQueue(Page page) {
+        BasicDBObject pageObject = new BasicDBObject("fb_id", page.getFacebookId())
+                .append("name", page.getName());
+        queueCollection.remove(pageObject);
     }
 
     public void insertComment(Comment comment) {
