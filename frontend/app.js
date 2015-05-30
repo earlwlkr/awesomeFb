e3ba56e111bddf8b73b2de5c439026f0f37d0b81
earlwlkr <mike358015@gmail.com>
2015-05-31 00:19:57 +0700

    Create mongoose model, jade view

--- frontend/app.js
+++ frontend/app.js
@@ -1,30 +1,45 @@
-var app = require('express')(),
+var express = require('express'),
+    app = express(),
+    path = require('path'),
     mongoose = require('mongoose');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongodb://localhost:27017/awesomefb');
+mongoose.connect('localhost', 'awesomefb');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function (callback) {
+
+  var commentSchema = new mongoose.Schema({ 
+    fb_id: String, 
+    message: String,
+    creator: {
+      fb_id: String,
+      name: String,
+      is_page: Boolean
+    },
+    time_created: Date,
+    topic: String,
+    is_spam: Boolean,
+    sentiment: String
+  });
+
+  commentSchema.statics.getAll = function(limit, cb) {
+    return this.find({}, cb).limit(limit);
+  }
+
+  var Comment = mongoose.model('Post', commentSchema);
   
   app.get('/', function(req, res) {
-    var collection = db.collection('posts');
-    
-    var obj = {};
-    
-    var options = {
-      'limit': 50
-    };
 
-    collection.find(obj, options).toArray(function(err, docs) {
-      var ret = docs;
+    Comment.getAll(20, function(err, docs) {
       res.render('layout',
-        { title : 'Home' }
-        );
-    });      
+        { comments : docs }
+      );
+    });
   });
 
   var server = app.listen(8080, function() {
