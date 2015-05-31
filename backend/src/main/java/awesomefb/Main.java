514060d68eaadfcd83c2fa75e9965648e2efc4a0
earlwlkr <mike358015@gmail.com>
2015-05-31 19:43:00 +0700

    Specify topic in Main

--- backend/src/main/java/awesomefb/Main.java
+++ backend/src/main/java/awesomefb/Main.java
@@ -11,8 +11,12 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("USAGE: java -jar [jar-name] [topic]");
+            return;
+        }
         // Topics: samsung, iphone, coffee, galaxy
         FacebookFocusedCrawler crawler = new FacebookFocusedCrawler();
-        crawler.run("samsung");
+        crawler.run(args[0]);
     }
 }
