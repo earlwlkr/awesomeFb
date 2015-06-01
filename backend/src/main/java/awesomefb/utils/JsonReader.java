3f1f83a4b51464827b309732b6ff30fd30d37223
earlwlkr <mike358015@gmail.com>
2015-06-01 07:22:16 +0700

    Set timeouts

--- backend/src/main/java/awesomefb/utils/JsonReader.java
+++ backend/src/main/java/awesomefb/utils/JsonReader.java
@@ -1,16 +1,23 @@
 package awesomefb.utils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonReader {
 
-    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        System.out.println("Waiting for HTTP response from " + url);
-        String jsonText = org.apache.commons.io.IOUtils.toString(new URL(url).openStream());
+    public static JSONObject readJsonFromUrl(String urlPath) throws IOException, JSONException {
+        System.out.println("Waiting for HTTP response from " + urlPath);
+        URL url = new URL(urlPath);
+        URLConnection con = url.openConnection();
+        con.setConnectTimeout(30000);
+        con.setReadTimeout(10000);
+        InputStream in = con.getInputStream();
+        String jsonText = org.apache.commons.io.IOUtils.toString(in);
         System.out.println("Received HTTP response.");
         JSONObject json = new JSONObject(jsonText);
         return json;
